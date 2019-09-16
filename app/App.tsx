4bac47f2ca3509304174b0f194a234e773bda668
Alex Rodin <contato@alexrodin.info>
2019-09-15 23:26:57 -0300

    Melhorias na documentação

--- app/App.tsx
+++ app/App.tsx
@@ -7,26 +7,25 @@ export default class App extends React.PureComponent {
     render() {
         return (
             <Blueprint
-                // image={require('./assets/github.png')}
-                // images={[
-                //     {
-                //         uri: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Profilewireframe.png'
-                //     },
-                //     require('./assets/wireframe-1.png'),
-                //     require('./assets/wireframe-2.png')
-                // ]}
-                imagesAsync={() => {
-                    const server = 'http://localhost:3000';
-                    return fetch(`${server}/images.json`)
-                        .then(resp => resp.json())
-                        .then(images => {
-                            images.forEach((image: any) => {
-                                image.uri = `${server}/${image.uri}`;
-                                image.thumb.uri = `${server}/${image.thumb.uri}`;
-                            });
-                            return images;
-                        });
-                }}
+                images={[
+                    {
+                        uri: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Profilewireframe.png'
+                    },
+                    require('./assets/wireframe-1.png'),
+                    require('./assets/wireframe-2.png')
+                ]}
+                // imagesAsync={() => {
+                //     const server = 'http://localhost:3000';
+                //     return fetch(`${server}/images.json`)
+                //         .then(resp => resp.json())
+                //         .then(images => {
+                //             images.forEach((image: any) => {
+                //                 image.uri = `${server}/${image.uri}`;
+                //                 image.thumb.uri = `${server}/${image.thumb.uri}`;
+                //             });
+                //             return images;
+                //         });
+                // }}
             >
                 <StatusBar backgroundColor={'transparent'} translucent={true} barStyle="dark-content"/>
                 <View style={[StyleSheet.absoluteFill, {justifyContent: 'center', alignItems: 'center'}]}>
